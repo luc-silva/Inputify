ee4305bcc2e92e749a6851d7270fbff8aaa2206b
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-16 17:43:47 -0300

    feat: add SelectInput component and css module

---
+++ src/components/SelectInput/SelectInput.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./SelectInput.module.css";
+
+interface ISelectInput extends ICustomInputProps {
+    options: any[];
+}
+export const SelectInput = ({
+    inputName,
+    label = false,
+    labelText,
+    placeholder = false,
+    placeholderText,
+    required = false,
+    options,
+    onChange,
+}: ISelectInput) => {
+    return (
+        <>
+            {label && (
+                <label htmlFor={inputName} className={styles[".label"]}>
+                    {labelText}
+                </label>
+            )}
+            <select
+                name={inputName}
+                className={styles[".input"]}
+                placeholder={placeholder ? placeholderText : undefined}
+                required={required}
+                onChange={onChange}
+            >
+                {options.map((value) => {
+                    return <option value={value}>{value}</option>;
+                })}
+            </select>
+        </>
+    );
+};
