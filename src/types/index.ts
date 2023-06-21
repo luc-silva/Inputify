1d5e06fd82e47daa3d5d41258d2632f74c2d2e1c
Lucas Santos <lucas.silva1203@outlook.com>
2023-06-21 18:29:33 -0300

    docs: JSDoc ICustomInputProps interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,15 @@
 import React from "react";
 
 /**
- * Base interface for props shared between multiples inputs.
+ * Properties from custom HTML input components.
+ * @interface ICustomInputProps
+ * @property {boolean} [placeholder=false] - Specifies whether a component will contain a placeholder.
+ * @property {string} [placeholderText] - Placeholder text.
+ * @property {boolean} [label=false] - Specifies whether a component will contain a label element.
+ * @property {string} [labelText] - Label text.
+ * @property {string} inputName - HTML element name.
+ * @property {boolean} [required=false] - Specifies whether the component is required.
+ * @property {React.ChangeEventHandler} onChange - Callback function called when the field value is changed.
  */
 export interface ICustomInputProps {
     placeholder?: boolean;
@@ -9,11 +17,11 @@ export interface ICustomInputProps {
     label?: boolean;
     labelText?: string;
     inputName: string;
-    required?:boolean;
-    onChange: React.ChangeEventHandler
+    required?: boolean;
+    onChange: React.ChangeEventHandler;
 }
 
 /**
  * Base type declarion for custom inputs.
  */
-export type ICustomInput<T> = (props:ICustomInputProps & T) => JSX.Element
+export type ICustomInput<T> = (props: ICustomInputProps & T) => JSX.Element;
